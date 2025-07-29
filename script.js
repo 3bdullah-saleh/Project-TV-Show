6b08e8f46a18a8ed0bfdf8e722eac763bc88f8c3
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-29 22:30:58 +0100

    add footer with TVMaze attribution link

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   allEpisodes.map(makePageForEpisodes);
+  renderFooter();
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  console.log("🚀 ~ makePageForEpisodes ~ rootElem:", episodeList);
   // render the title including name, season, and episode number
   const { name, season, number } = episodeList;
   const header = document.createElement("h3");
@@ -24,6 +24,16 @@ function makePageForEpisodes(episodeList) {
   const episodeSummary = document.createElement("p");
   episodeSummary.innerHTML = summary;
   rootElem.appendChild(episodeSummary);
+}
+
+function renderFooter() {
+  const footer = document.createElement("footer");
+  const link = document.createElement("a");
+  link.href = "https://www.tvmaze.com/";
+  link.target = "blank"
+  link.textContent = "Data originally from TVMaze.com";
+  footer.appendChild(link);
+  document.body.appendChild(footer)
 }
 
 window.onload = setup;
