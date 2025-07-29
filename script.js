827432ae855f1f861240fe09e4c635cf87cc89e4
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-29 20:38:41 +0100

    render one episode with title including name, season, and episode number

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-//You can edit ALL of the code here
+// Godal: render one episode in the user interface
+
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const oneEpisodes = getOneEpisode();
+  makePageForEpisodes(oneEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const {name, season, number} = episodeList;
+  const header = document.createElement("h3");
+  header.textContent = `${name} - S${season.toString().padStart(2, "0")}E${number.toString().padStart(2, "0")}`;
+  rootElem.appendChild(header);
 }
 
 window.onload = setup;
+
+
+/*
+1. All episodes must be shown 
+2. For each episode, _at least_ following must be displayed:
+   1. The name of the episode
+   2. The season number
+   3. The episode number
+   4. The medium-sized image for the episode
+*/
+
+// create an element 
+// append title to that element 
+// append it to the body
